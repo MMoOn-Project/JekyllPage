beb6b3ae5a3169cb05c76502e2a72fde21565332
Natanael Arndt <arndtn@gmail.com>
2018-11-24 18:06:31 +0100

    fix script

--- provisioning/createRdfFiles.sh
+++ provisioning/createRdfFiles.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
-EXTRACT = $PWD/../provisioning/extract.py
-INVENTORY = $PWD/../_data/heb_inventory.ttl
-SCHEMA_FOLDER = $PWD/schema/oh/
-INVENTORY_FOLDER = $PWD/inventory/oh/
+
+EXTRACT=$PWD/../provisioning/extract.py
+INVENTORY=$PWD/../_data/heb_inventory.ttl
+SCHEMA_FOLDER=$PWD/schema/oh/
+INVENTORY_FOLDER=$PWD/inventory/oh/
 
 $EXTRACT --file $INVENTORY --base "http://mmoon.org/lang/heb/"
 echo "Convert schema TTLs to JSON+RDF"
 cd $SCHEMA_FOLDER
-for res in *.ttl; rapper -i turtle -o json $res > ${res%.ttl}.json 2> /dev/null ; done
+for res in *.ttl; do rapper -i turtle -o json $res > ${res%.ttl}.json 2> /dev/null ; done
 echo "Convert inventory TTLs to JSON+RDF"
 cd $INVENTORY_FOLDER
-for res in *.ttl; rapper -i turtle -o json $res > ${res%.ttl}.json 2> /dev/null ; done
+for res in *.ttl; do rapper -i turtle -o json $res > ${res%.ttl}.json 2> /dev/null ; done
